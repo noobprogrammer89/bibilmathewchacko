d7a86392b6e5c2b2cfbd1e7dcbeaed807d7dff6e
Bibil Mathew Chacko <bibil@bestdocapp.com>
2019-10-05 07:49:24 +0530

    first commit

---
+++ src/main/java/bibilmathewchacko/BibilmathewchackoApplication.java
@@ -0,0 +1,13 @@
+package bibilmathewchacko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibilmathewchackoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibilmathewchackoApplication.class, args);
+	}
+
+}
